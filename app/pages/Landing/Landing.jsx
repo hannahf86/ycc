482b2e9eadb5d5b1f98fa4643c6b97b14104eb55
hannahf86 <hannahfeehan.design@gmail.com>
2025-01-16 09:57:58 +0000

    landing responsive design complete

--- app/pages/Landing/Landing.jsx
+++ app/pages/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 // NEXT
 import Image from "next/image";
+import Link from "next/link";
 
 // STYLES
 import styles from "./landing.module.css";
@@ -19,6 +20,20 @@ const Landing = () => {
         className={styles.hero}
         alt="an artistic photo of a choir singing and having fun"
       />
+
+      <h1 className={styles.title}>
+        York <br /> Community
+        <br /> Choir
+      </h1>
+
+      <div className={styles.cta}>
+        <h2 className={styles.subtitle}>
+          Experience the joy of singing in a friendly and welcoming activity
+        </h2>
+        <Link href="#">
+          <button className={styles.button}>Join Us</button>
+        </Link>
+      </div>
     </div>
   );
 };
